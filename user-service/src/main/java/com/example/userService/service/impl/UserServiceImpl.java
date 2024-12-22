c63abddc6be76d0b967aeaac8775526eaced7810
shikhasingh1999 <shikha1266@gmail.com>
2024-12-22 22:42:45 +0530

    ninth commit

--- user-service/src/main/java/com/example/userService/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userService/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
