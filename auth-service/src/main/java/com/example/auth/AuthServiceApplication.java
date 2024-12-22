328ff416a70c1bf0239e7e57bf8683783aced80a
shikhasingh1999 <shikha1266@gmail.com>
2024-12-22 22:47:55 +0530

    feign bean not detecting fix

--- auth-service/src/main/java/com/example/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/auth/AuthServiceApplication.java
@@ -3,11 +3,12 @@ package com.example.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"com.example.gateway"})
+@EnableFeignClients
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
