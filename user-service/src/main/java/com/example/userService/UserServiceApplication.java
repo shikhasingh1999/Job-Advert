258aff2b115cae897cba07f0765675d024f68a92
shikhasingh1999 <shikha1266@gmail.com>
2024-12-23 22:00:52 +0530

    enable feign

--- user-service/src/main/java/com/example/userService/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userService/UserServiceApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @ComponentScan(basePackages = {"com.example.common", "com.example.userService"})
 public class UserServiceApplication implements CommandLineRunner {
 
