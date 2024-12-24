bacbafbcd31e9b982123a47717ee3c50194a6391
shikhasingh1999 <shikha1266@gmail.com>
2024-12-24 20:58:46 +0530

    AuthConfig bean & bypass swagger url from security authentication

---
+++ auth-service/src/main/java/com/example/auth/config/AuthConfig.java
@@ -0,0 +1,40 @@
+package com.example.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    /**
+     * By-pass authentication for the matched urls
+     * @return
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer () {
+        return web -> web.ignoring().requestMatchers("/v1/auth/**",
+                "/swagger-resources/**",
+                "/swagger-ui.html/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**",
+                "/v3/api-docs/**");
+    }
+
+
+
+}
