c967abbd4eab49ccb4d3d83e316e9390660f987f
shikhasingh1999 <shikha1266@gmail.com>
2024-12-24 20:56:19 +0530

    AuthService bean error fix

--- auth-service/src/main/java/com/example/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/example/auth/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private AuthService authService;
+    private final AuthService authService;
 
     @PostMapping("/register")
     public ResponseEntity<RegisterDto> register(@RequestBody RegisterRequest registerRequest) {
