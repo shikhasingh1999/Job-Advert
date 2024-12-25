d2548d6cb6d6c0d3a3f2eff89b2423608b5e83ca
shikhasingh1999 <shikha1266@gmail.com>
2024-12-25 23:05:29 +0530

    openApi config

---
+++ job-service/src/main/java/com/example/jobService/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+package com.example.jobService.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI (@Value("${application-title}") String title,
+                            @Value("${application-description}") String description,
+                            @Value("${application-version}") String version,
+                            @Value("${application-license}") String license) {
+
+        return new OpenAPI()
+                .info(new Info()
+                    .title(title)
+                    .description(description)
+                    .version(version)
+                    .license(new License().name(license)))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())
+                );
+
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .type(SecurityScheme.Type.HTTP);
+
+    }
+}
