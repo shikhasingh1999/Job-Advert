9ea8002801455f616aaa920bbabd2909393e9184
shikhasingh1999 <shikha1266@gmail.com>
2025-01-12 20:21:59 +0530

    EnableMethodSecurity annotation to enable @PreAuthorize method

--- user-service/src/main/java/com/example/userService/config/SecurityConfig.java
+++ user-service/src/main/java/com/example/userService/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.userService.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -11,6 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(
+        securedEnabled = true,    // Enables @Secured annotations
+        prePostEnabled = true,    // Enables @PreAuthorize and @PostAuthorize annotations
+        jsr250Enabled = true      // Enables @RolesAllowed annotations (JSR-250)
+)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
