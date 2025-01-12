f0a8fc6d86dde0be3f8bec3a86edddf9131371c7
shikhasingh1999 <shikha1266@gmail.com>
2025-01-12 22:42:25 +0530

    CommandLineRunner info

--- user-service/src/main/java/com/example/userService/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userService/UserServiceApplication.java
@@ -43,4 +43,24 @@ public class UserServiceApplication implements CommandLineRunner {
 
 	}
 
+//	the method signature public void run(String... args) is typically used as
+//	part of the CommandLineRunner or ApplicationRunner interfaces. These interfaces
+//	are provided by Spring Boot to allow you to execute code after the Spring
+//	application context is fully initialized.
+
+//	the run method is the main method that gets executed after the application has started
+
+//	The args parameter contains any command-line arguments passed to the application.
+
+//  Uses --->
+//	Initialization Logic: It's commonly used for running startup logic
+//	(e.g., loading data into a database, performing calculations, or running
+//	initial setups) right after the application context is initialized.
+//	Command-Line Arguments: The args array lets you access any command-line
+//	arguments passed to the application when starting it. This is useful for
+//	applications where arguments influence behavior, such as batch jobs or
+//	other tools that need to be configured via the command line.
+// Application startup tasks: Running tasks like checking system health,
+// logging initial messages, or performing an initial setup (like loading
+// configuration data from files).
 }
